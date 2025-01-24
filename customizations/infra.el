96f711acfa7834748c3fb4bb58c98c52afc55cc0
Alex Tsariounov <alex.tsariounov@hpe.com>
2025-01-24 11:59:04 -0700

    Add infra.el and lang.el customizations files
    Signed-off-by: Alex Tsariounov <alex.tsariounov@hpe.com>

---
+++ customizations/infra.el
@@ -0,0 +1,11 @@
+;; Infrastructure modes
+
+;; dockerfile mode
+(setup (:package dockerfile-mode))
+
+;; docker compose mode
+(setup (:package docker-compose-mode))
+
+;; terraform
+(setup (:package terraform-mode))
+
